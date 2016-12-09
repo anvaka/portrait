46bec9a886bec616cf6fc3028d6300b3f8280bc4
anvaka <anvaka@gmail.com>
2016-12-09 08:50:23 -0800

    Raster rendering

--- README.md
+++ README.md
@@ -1,13 +1,43 @@
-# Portrait
+[# Portrait](https://anvaka.github.io/portrait/)
 
 I took this picture of Mark Twain:
 
 ![mark twain](https://raw.githubusercontent.com/anvaka/portrait/master/docs/mark_twain_small.png)
 
-And pixelated it into this with a small twist...
+Pixelated it into this:
 
 ![mark twain pixelated](https://raw.githubusercontent.com/anvaka/portrait/master/docs/mark_twain_cloud.png)
 
 When you zoom closer you will see quotes from Mark Twain:
 
 ![mark twain twist](https://raw.githubusercontent.com/anvaka/portrait/master/docs/mark_twist.gif)
+
+Explore it live here: https://anvaka.github.io/portrait/
+
+# Have an advice?
+
+If you'd like to explore other portraits in a similar way or have a suggestion
+how to make this better, please reach out to me:
+
+* open issue here: https://github.com/anvaka/portrait/issues 
+* email to me (anvaka@gmail.com)
+* Tweet to me: https://twitter.com/anvaka
+
+## How does this work?
+
+I wrote down more than 200 Mark Twain's quotes. Mostly quotes came from
+[reddit](https://www.reddit.com/r/QuotesPorn/search?q=mark+twain&restrict_sr=on&sort=relevance&t=all)
+and [good reads](https://www.goodreads.com/author/quotes/1244.Mark_Twain).
+
+Then I sorted them according to their popularity (e.g. by number of votes on reddit,
+or likes on good reads), and dropped them onto canvas.
+
+Once I got the canvas, I saved it into high-resolution png file (8,000 x 8,000 pixels).
+
+* I used [OpenSeadragon](https://openseadragon.github.io) to have pan and zoom support.
+* When URL changes, I update query string, so that you can share favorite quotes.
+The query string state is managed by [query-state](https://github.com/anvaka/query-state)
+
+# Enjoy!
+
+That's it! Hope you enjoy exploring Mark Twain's wisdom in a new, artistic way!
--- index.js
+++ index.js
@@ -1,94 +1,58 @@
-/* global panzoom, queryState */
-var zoomContainer = document.getElementById('scene')
-var zoomer = panzoom(zoomContainer)
-
-var width = window.innerWidth;
-var height = window.innerHeight;
-
-var qs = queryState();
-setInitialCoordinates();
-listenToEvents();
-
 var lastRequest = 0;
 var saveDelay = 200; // Don't want to spam with history updates
 
-function setInitialCoordinates() {
-  var transform = getTransformFromQueryState(qs);
-  if (transform) {
-    var dw = transform.w - window.innerWidth;
-    var dh = transform.h - window.innerHeight;
-    zoomer.zoomAbs(0, 0, transform.scale);
-    zoomer.moveTo(transform.x - dw * 0.5, transform.y - dh * 0.5);
-  } else {
-    centerScene();
+var queryState = require('query-state');
+require('openseadragon'); // it adds itself to window.
+
+var qs = queryState({
+  index: 'mark_twain'
+});
+
+var indexName = qs.get('index')
+
+var viewer = window.OpenSeadragon({
+  id: 'map',
+  showNavigationControl: false,
+  prefixUrl: '',
+  preserveViewport: true,
+  tileSources: 'https://anvaka.github.io/portrait-data/' + indexName + '/index.dzi'
+});
+
+window.resetZoom = function() {
+  viewer.viewport.goHome();
+}
+
+var box = getBox();
+
+viewer.addHandler('open', function() {
+  if (box) {
+    viewer.viewport.fitBounds(box, true)
   }
+});
+
+viewer.addHandler('viewport-change', function() {
+  var box = viewer.viewport.getBounds()
+  saveCurrentBox(box);
+});
+
+function getBox() {
+  var degrees = qs.get('degrees') || 0;
+  var height = qs.get('height');
+  var width = qs.get('width');
+  var x = qs.get('x');
+  var y = qs.get('y');
+  if (x === undefined || y === undefined || width === undefined || height === undefined) return;
+
+  return new window.OpenSeadragon.Rect(x, y, width, height, degrees)
 }
 
-function resetZoom() {
-  window.location.hash = '';
-  window.location.reload();
-}
-
-function listenToEvents() {
-  window.addEventListener('resize', onWindowResize, false);
-
-  zoomContainer.addEventListener('panstart', panZoomChanged);
-  zoomContainer.addEventListener('zoom', panZoomChanged);
-  zoomContainer.addEventListener('pan', panZoomChanged);
-}
-
-function onWindowResize() {
-  width = window.innerWidth;
-  height = window.innerHeight;
-}
-
-function panZoomChanged() {
-  saveCurrentTransform(zoomer.getTransform(), qs);
-}
-
-function saveCurrentTransform(transform, queryState) {
+function saveCurrentBox(box) {
   if (lastRequest) {
     clearTimeout(lastRequest);
   }
 
   lastRequest = setTimeout(function() {
-    queryState.set({
-      x: round(transform.x),
-      y: round(transform.y),
-      w: window.innerWidth,
-      h: window.innerHeight,
-      scale: round(transform.scale),
-    });
+    qs.set(box);
   }, saveDelay);
 }
 
-function round(x) {
-  return Math.round(x * 1000)/1000; 
-}
-
-function centerScene() {
-  var sceneRect = zoomContainer.getBBox()
-  zoomer.moveBy(-sceneRect.x, -sceneRect.y)
-
-  var scale = Math.min(width, height) / Math.max(sceneRect.width, sceneRect.height)
-
-  var dx = (width - sceneRect.width * scale) / 2;
-  var dy = (height - sceneRect.height * scale) / 2;
-
-  zoomer.zoomAbs(0, 0, scale)
-  zoomer.moveBy(dx, dy)
-}
-
-function getTransformFromQueryState(query) {
-  var x = parseFloat(query.get('x'));
-  var y = parseFloat(query.get('y'));
-  var scale = parseFloat(query.get('scale'));
-  if (Number.isNaN(x) || Number.isNaN(y) || Number.isNaN(scale)) return;
-
-  var w = parseInt(query.get('w'), 10);
-  var h = parseInt(query.get('h'), 10);
-
-  if (Number.isNaN(w)) w = window.innerWidth;
-  if (Number.isNaN(h)) h = window.innerHeight;
-  return { x: x, y: y, scale: scale, w: w, h: h};
-}
